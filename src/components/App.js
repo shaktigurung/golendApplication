e306733f031ac073651272f416d8041b53e4401f
Shakti Gurung <mr.shakti.gurung@gmail.com>
2019-12-16 13:18:26 +1100

    url loader and logo added

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,16 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Years from './Years/Years';
 import Vehicles from './Vehicles/Vehicles';
 import NotFound from './NotFound/NotFound';
+import Header from './Header/Header';
+import './App.css';
 
 class App extends Component {
 
     render() {
         return (
-            <div>
+            <div className="container">
                 <BrowserRouter>
+                    <Header />
                     <Switch>
                         <Route exact path="/" component = {Years} />
                         <Route exact path="/years" component = {Years} />
